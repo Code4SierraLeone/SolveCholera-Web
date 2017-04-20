659a4c98a9b33d33be77068ce8a8812412b31824
Samson Paul <samsonpaul4@gmail.com>
2017-04-20 08:34:32 +0300

    removes extra routers

--- src/js/config.router.js
+++ src/js/config.router.js
@@ -41,22 +41,9 @@ angular.module('app')
                   templateUrl: 'tpl/app_dashboard_v2.html',
                   resolve: load(['js/controllers/chart.js'])
               })
-              .state('app.dashboard-v3', {
-                  url: '/dashboard-v3',
-                  templateUrl: 'tpl/app_dashboard_v3.html',
-                  resolve: load(['js/controllers/chart.js'])
-              })
               .state('app.ui', {
                   url: '/ui',
                   template: '<div ui-view class="fade-in-up"></div>'
-              })
-              .state('app.ui.buttons', {
-                  url: '/buttons',
-                  templateUrl: 'tpl/ui_buttons.html'
-              })
-              .state('app.ui.icons', {
-                  url: '/icons',
-                  templateUrl: 'tpl/ui_icons.html'
               })
               .state('app.ui.grid', {
                   url: '/grid',
@@ -87,25 +74,10 @@ angular.module('app')
                   url: '/timeline',
                   templateUrl: 'tpl/ui_timeline.html'
               })
-              .state('app.ui.tree', {
-                  url: '/tree',
-                  templateUrl: 'tpl/ui_tree.html',
-                  resolve: load(['angularBootstrapNavTree', 'js/controllers/tree.js'])
-              })
               .state('app.ui.toaster', {
                   url: '/toaster',
                   templateUrl: 'tpl/ui_toaster.html',
                   resolve: load(['toaster', 'js/controllers/toaster.js'])
-              })
-              .state('app.ui.jvectormap', {
-                  url: '/jvectormap',
-                  templateUrl: 'tpl/ui_jvectormap.html',
-                  resolve: load('js/controllers/vectormap.js')
-              })
-              .state('app.ui.googlemap', {
-                  url: '/googlemap',
-                  templateUrl: 'tpl/ui_googlemap.html',
-                  resolve: load(['js/app/map/load-google-maps.js', 'js/app/map/ui-map.js', 'js/app/map/map.js'], function(){ return loadGoogleMaps(); })
               })
               .state('app.chart', {
                   url: '/chart',
@@ -113,38 +85,6 @@ angular.module('app')
                   resolve: load('js/controllers/chart.js')
               })
               // table
-              .state('app.table', {
-                  url: '/table',
-                  template: '<div ui-view></div>'
-              })
-              .state('app.table.static', {
-                  url: '/static',
-                  templateUrl: 'tpl/table_static.html'
-              })
-              .state('app.table.datatable', {
-                  url: '/datatable',
-                  templateUrl: 'tpl/table_datatable.html'
-              })
-              .state('app.table.footable', {
-                  url: '/footable',
-                  templateUrl: 'tpl/table_footable.html'
-              })
-              .state('app.table.grid', {
-                  url: '/grid',
-                  templateUrl: 'tpl/table_grid.html',
-                  resolve: load(['ngGrid','js/controllers/grid.js'])
-              })
-              .state('app.table.uigrid', {
-                  url: '/uigrid',
-                  templateUrl: 'tpl/table_uigrid.html',
-                  resolve: load(['ui.grid','js/controllers/uigrid.js'])
-              })
-              .state('app.table.editable', {
-                  url: '/editable',
-                  templateUrl: 'tpl/table_editable.html',
-                  controller: 'XeditableCtrl',
-                  resolve: load(['xeditable','js/controllers/xeditable.js'])
-              })
               .state('app.table.smart', {
                   url: '/smart',
                   templateUrl: 'tpl/table_smart.html',
@@ -155,57 +95,6 @@ angular.module('app')
                   url: '/form',
                   template: '<div ui-view class="fade-in"></div>',
                   resolve: load('js/controllers/form.js')
-              })
-              .state('app.form.components', {
-                  url: '/components',
-                  templateUrl: 'tpl/form_components.html',
-                  resolve: load(['ngBootstrap','daterangepicker','js/controllers/form.components.js'])
-              })
-              .state('app.form.elements', {
-                  url: '/elements',
-                  templateUrl: 'tpl/form_elements.html'
-              })
-              .state('app.form.validation', {
-                  url: '/validation',
-                  templateUrl: 'tpl/form_validation.html'
-              })
-              .state('app.form.wizard', {
-                  url: '/wizard',
-                  templateUrl: 'tpl/form_wizard.html'
-              })
-              .state('app.form.fileupload', {
-                  url: '/fileupload',
-                  templateUrl: 'tpl/form_fileupload.html',
-                  resolve: load(['angularFileUpload','js/controllers/file-upload.js'])
-              })
-              .state('app.form.imagecrop', {
-                  url: '/imagecrop',
-                  templateUrl: 'tpl/form_imagecrop.html',
-                  resolve: load(['ngImgCrop','js/controllers/imgcrop.js'])
-              })
-              .state('app.form.select', {
-                  url: '/select',
-                  templateUrl: 'tpl/form_select.html',
-                  controller: 'SelectCtrl',
-                  resolve: load(['ui.select','js/controllers/select.js'])
-              })
-              .state('app.form.slider', {
-                  url: '/slider',
-                  templateUrl: 'tpl/form_slider.html',
-                  controller: 'SliderCtrl',
-                  resolve: load(['vr.directives.slider','js/controllers/slider.js'])
-              })
-              .state('app.form.editor', {
-                  url: '/editor',
-                  templateUrl: 'tpl/form_editor.html',
-                  controller: 'EditorCtrl',
-                  resolve: load(['textAngular','js/controllers/editor.js'])
-              })
-              .state('app.form.xeditable', {
-                  url: '/xeditable',
-                  templateUrl: 'tpl/form_xeditable.html',
-                  controller: 'XeditableCtrl',
-                  resolve: load(['xeditable','js/controllers/xeditable.js'])
               })
               // pages
               .state('app.page', {
@@ -284,27 +173,6 @@ angular.module('app')
                   resolve: load(['moment','fullcalendar','ui.calendar','js/app/calendar/calendar.js'])
               })
 
-              // mail
-              .state('app.mail', {
-                  abstract: true,
-                  url: '/mail',
-                  templateUrl: 'tpl/mail.html',
-                  // use resolve to load other dependences
-                  resolve: load( ['js/app/mail/mail.js','js/app/mail/mail-service.js','moment'] )
-              })
-              .state('app.mail.list', {
-                  url: '/inbox/{fold}',
-                  templateUrl: 'tpl/mail.list.html'
-              })
-              .state('app.mail.detail', {
-                  url: '/{mailId:[0-9]{1,4}}',
-                  templateUrl: 'tpl/mail.detail.html'
-              })
-              .state('app.mail.compose', {
-                  url: '/compose',
-                  templateUrl: 'tpl/mail.new.html'
-              })
-
               .state('layout', {
                   abstract: true,
                   url: '/layout',
@@ -360,62 +228,6 @@ angular.module('app')
                   templateUrl: 'tpl/apps_contact.html',
                   resolve: load( ['js/app/contact/contact.js'] )
               })
-              .state('app.weather', {
-                  url: '/weather',
-                  templateUrl: 'tpl/apps_weather.html',
-                  resolve: load(['js/app/weather/skycons.js','angular-skycons','js/app/weather/ctrl.js','moment'])
-              })
-              .state('app.todo', {
-                  url: '/todo',
-                  templateUrl: 'tpl/apps_todo.html',
-                  resolve: load(['js/app/todo/todo.js', 'moment'])
-              })
-              .state('app.todo.list', {
-                  url: '/{fold}'
-              })
-              .state('app.note', {
-                  url: '/note',
-                  templateUrl: 'tpl/apps_note_material.html',
-                  resolve: load(['js/app/note/note.js', 'moment'])
-              })
-              .state('music', {
-                  url: '/music',
-                  templateUrl: 'tpl/music.html',
-                  controller: 'MusicCtrl',
-                  resolve: load([
-                            'com.2fdevs.videogular', 
-                            'com.2fdevs.videogular.plugins.controls', 
-                            'com.2fdevs.videogular.plugins.overlayplay',
-                            'com.2fdevs.videogular.plugins.poster',
-                            'com.2fdevs.videogular.plugins.buffering',
-                            'js/app/music/ctrl.js', 
-                            'js/app/music/theme.css'
-                          ])
-              })
-                  .state('music.home', {
-                      url: '/home',
-                      templateUrl: 'tpl/music.home.html'
-                  })
-                  .state('music.genres', {
-                      url: '/genres',
-                      templateUrl: 'tpl/music.genres.html'
-                  })
-                  .state('music.detail', {
-                      url: '/detail',
-                      templateUrl: 'tpl/music.detail.html'
-                  })
-                  .state('music.mtv', {
-                      url: '/mtv',
-                      templateUrl: 'tpl/music.mtv.html'
-                  })
-                  .state('music.mtvdetail', {
-                      url: '/mtvdetail',
-                      templateUrl: 'tpl/music.mtv.detail.html'
-                  })
-                  .state('music.playlist', {
-                      url: '/playlist/{fold}',
-                      templateUrl: 'tpl/music.playlist.html'
-                  })
               .state('app.material', {
                   url: '/material',
                   template: '<div ui-view class="wrapper-md"></div>',
@@ -424,30 +236,6 @@ angular.module('app')
                 .state('app.material.button', {
                   url: '/button',
                   templateUrl: 'tpl/material/button.html'
-                })
-                .state('app.material.color', {
-                  url: '/color',
-                  templateUrl: 'tpl/material/color.html'
-                })
-                .state('app.material.icon', {
-                  url: '/icon',
-                  templateUrl: 'tpl/material/icon.html'
-                })
-                .state('app.material.card', {
-                  url: '/card',
-                  templateUrl: 'tpl/material/card.html'
-                })
-                .state('app.material.form', {
-                  url: '/form',
-                  templateUrl: 'tpl/material/form.html'
-                })
-                .state('app.material.list', {
-                  url: '/list',
-                  templateUrl: 'tpl/material/list.html'
-                })
-                .state('app.material.ngmaterial', {
-                  url: '/ngmaterial',
-                  templateUrl: 'tpl/material/ngmaterial.html'
                 });
 
           function load(srcs, callback) {
